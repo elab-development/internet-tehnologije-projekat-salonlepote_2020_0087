40d21a2c272d58fd113579316dd96fbf2484dbbe
jefimijaristic <risticjefimija1@gmail.com>
2024-02-02 00:31:02 +0100

    sortiranje

--- reactprojekat/src/komponente/usluge/cenovnik.jsx
+++ reactprojekat/src/komponente/usluge/cenovnik.jsx
@@ -4,11 +4,20 @@ import useUsluge from '../rezervacije/hooks/useUsluge';
 function Cenovnik() {
   const [usluge] = useUsluge();
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortDirection, setSortDirection] = useState('asc');
   const uslugePerPage = 5;
+
+  const sortedUsluge = usluge.slice().sort((a, b) => {
+    if (sortDirection === 'asc') {
+      return a.cena - b.cena;
+    } else {
+      return b.cena - a.cena;
+    }
+  });
 
   const indexOfLastUsluga = currentPage * uslugePerPage;
   const indexOfFirstUsluga = indexOfLastUsluga - uslugePerPage;
-  const currentUsluge = usluge.slice(indexOfFirstUsluga, indexOfLastUsluga);
+  const currentUsluge = sortedUsluge.slice(indexOfFirstUsluga, indexOfLastUsluga);
 
   const totalPages = Math.ceil(usluge.length / uslugePerPage);
 
@@ -16,13 +25,20 @@ function Cenovnik() {
     setCurrentPage(newPage);
   };
 
+  const handleSortChange = () => {
+    setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+  };
+
   return (
     <div>
       <h2>Cenovnik Usluga</h2>
+      <button onClick={handleSortChange}>
+        Sortiraj po ceni ({sortDirection === 'asc' ? 'Rastuće' : 'Opadajuće'})
+      </button>
       <table>
         <thead>
           <tr>
-          <th>ID</th>
+            <th>ID</th>
             <th>Naziv Usluge</th>
             <th>Cena</th>
             <th>Opis</th>
@@ -31,7 +47,7 @@ function Cenovnik() {
         <tbody>
           {currentUsluge.map((usluga) => (
             <tr key={usluga.id}>
-                 <td>{usluga.id}</td>
+              <td>{usluga.id}</td>
               <td>{usluga.naziv}</td>
               <td>{usluga.cena}</td>
               <td>{usluga.opis}</td>

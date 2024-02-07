9add92d05ee9361eecc73338a1d13eb77866803f
katarinastijepovic <stijepovic55@gmail.com>
2024-02-07 01:10:35 +0100

    prepravljena komponenta tako da prikazuje danasnje i sutrasnje rezervacije odvojeno

--- reactprojekat/src/komponente/rezervacije/zaposleni/MojeRezervacije.jsx
+++ reactprojekat/src/komponente/rezervacije/zaposleni/MojeRezervacije.jsx
@@ -17,7 +17,14 @@ function Mojerezervacije() {
     // Izvrši Axios GET zahtev za rezervacijama zaposlenog sa dodatim zaglavljem
     axios.get('http://127.0.0.1:8000/api/rezervacije/getReservationsByEmployee', { headers })
       .then((response) => {
-        setReservations(response.data.data);  
+        const sortedReservations = response.data.data.sort((a, b) => {
+          // Sortiraj rezervacije po datumu i vremenu
+          const dateA = new Date(a.datum + ' ' + a.vreme);
+          const dateB = new Date(b.datum + ' ' + b.vreme);
+          return dateA - dateB;
+        });
+
+        setReservations(sortedReservations);  
         setLoading(false);
       })
       .catch((error) => {
@@ -30,31 +37,49 @@ function Mojerezervacije() {
     return <p>Učitavanje rezervacija...</p>;
   }
 
+  // Razdvajamo rezervacije prema datumu
+  const todayReservations = reservations.filter((reservation) => {
+    const today = new Date();
+    const reservationDate = new Date(reservation.datum);
+    return reservationDate.toDateString() === today.toDateString();
+  });
+
+  const tomorrowReservations = reservations.filter((reservation) => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const reservationDate = new Date(reservation.datum);
+    return reservationDate.toDateString() === tomorrow.toDateString();
+  });
+
   return (
     <div>
       <h2>Moje Rezervacije</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Datum</th>
-            <th>Vreme</th>
-            <th>Korisnik</th>
-            <th>Usluga</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservations.map((reservation) => (
+      <div className="column">
+        <h3>Današnje rezervacije</h3>
+        <table>
+          {/* Prikazujemo današnje rezervacije */}
+          {todayReservations.map((reservation) => (
             <tr key={reservation.id}>
-              <td>{reservation.id}</td>
-              <td>{reservation.datum}</td>
               <td>{reservation.vreme}</td>
               <td>{reservation.korisnik.name}</td>
               <td>{reservation.usluga.naziv}</td>
             </tr>
           ))}
-        </tbody>
-      </table>
+        </table>
+      </div>
+      <div className="column">
+        <h3>Sutrašnje rezervacije</h3>
+        <table>
+          {/* Prikazujemo sutrašnje rezervacije */}
+          {tomorrowReservations.map((reservation) => (
+            <tr key={reservation.id}>
+              <td>{reservation.vreme}</td>
+              <td>{reservation.korisnik.name}</td>
+              <td>{reservation.usluga.naziv}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 }
